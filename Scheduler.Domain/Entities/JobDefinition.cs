d3d900d9305f2bcf1e46926510a09e68e1f47971
Doug Vanderstoep <doug.vanderstoep@car-mart.com>
2022-03-07 21:51:52 -0600

    DataAnnotations

--- Scheduler.Domain/Entities/JobDefinition.cs
+++ Scheduler.Domain/Entities/JobDefinition.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Scheduler.Domain
 {
     public class JobDefinition : SoftDeleteEntity
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string AssemblyName { get; set; }
+        [Required]
         public string MethodName { get; set; }
+        [Required]
         public string CronExpression { get; set; }
         public bool IsPriority { get; set; }
     }
